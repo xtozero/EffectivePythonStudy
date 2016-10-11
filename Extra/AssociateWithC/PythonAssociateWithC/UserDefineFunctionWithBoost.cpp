e95b3587dad5f87c9903ef565b99e812823da3d6
xtozero <xtozero@naver.com>
2016-10-12 02:30:11 +0900

    Fix boost example error

--- Extra/AssociateWithC/PythonAssociateWithC/UserDefineFunctionWithBoost.cpp
+++ Extra/AssociateWithC/PythonAssociateWithC/UserDefineFunctionWithBoost.cpp
@@ -4,31 +4,27 @@
 
 using namespace boost::python;
 
+// 부스트는 파이썬 3에서 안됍니다....
 int main( )
 {
-	Py_SetProgramName( L"UserDefineFunction" );
+	//Py_SetProgramName( L"UserDefineFunctionWithBoost" );
 	Py_Initialize( );
-
-	object module;
-	object func;
-	object args;
-	object returnValue;
 
 	PyRun_SimpleString(
 		"import sys\n"
 		"sys.path.append('.')\n"
 	);
 
-	module = object( handle<>( PyImport_ImportModule( "Calc" ) ) );
-	func = object( handle<>( PyObject_GetAttrString( module.ptr(), "add" ) ) );
-	args = object( handle<>( PyTuple_New( 2 ) ) );
+	object module = object( handle<>( PyImport_ImportModule( "Calc" ) ) );
+	object func = object( handle<>( PyObject_GetAttrString( module.ptr(), "add" ) ) );
+	object args = object( handle<>( PyTuple_New( 2 ) ) );
 
 	// PyTuple_SetItem function steals a reference
 	PyTuple_SetItem( args.ptr(), 0, PyLong_FromLong( 5 ) );
 	PyTuple_SetItem( args.ptr(), 1, PyLong_FromLong( 10 ) );
 
-	returnValue = object( handle<>( PyObject_CallObject( func.ptr(), args.ptr() ) ) );
-	std::cout << extract<int>( returnValue ) << std::endl;
+	object returnValue = object( handle<>( PyObject_CallObject( func.ptr(), args.ptr() ) ) );
+	std::cout << extract<long>( returnValue ) << std::endl;
 
 	Py_Finalize( );
 
